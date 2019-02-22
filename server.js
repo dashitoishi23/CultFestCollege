14b81a562e711be369083ac8acd36abc3b27d01e
Hitoishi <31213720+Hitoishi@users.noreply.github.com>
2019-02-23 00:08:43 +0530

    Second commit

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const mongoose=require('mongoose');
 const db=require('./config/keys').mongoUrl;
 const bodyParser=require('body-parser');
 const users=require('./routes/api/users');
+const path = require('path');
 
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 mongoose.connect(db,{
     useNewUrlParser: true
@@ -23,10 +25,17 @@ mongoose.connect(db,{
 
 app.use('/api/users',users);
 
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+    
+}
 
 
 const port=process.env.PORT||5000;
+
 
 app.listen(port,()=>{
     console.log(`server connected at ${port}`);
